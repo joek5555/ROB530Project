bc5394c8caf27e5c73015fc6c40f669c5e3029c7
Joseph Kennedy <joekennedy5555@gmail.com>
2023-04-04 16:02:43 -0400

    comment out some prints

--- PF_landmarks/plot.py
+++ PF_landmarks/plot.py
@@ -20,8 +20,8 @@ def plot(groundtruth_x, groundtruth_y, landmarks_x, landmarks_y, robot1_system):
 
             landmark_particles_x = landmark_particles[:,0]
             landmakr_particles_y = landmark_particles[:,1]
-            print(landmark_particles_x)
-            print(landmakr_particles_y)
+            #print(landmark_particles_x)
+            #print(landmakr_particles_y)
             ax.scatter(landmark_particles_x, landmakr_particles_y, s=1, c='g')
 
                 
