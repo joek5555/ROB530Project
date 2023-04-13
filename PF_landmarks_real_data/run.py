f4f7e907e6be3813d454d8bf7c3904f08ebc5355
Joseph Kennedy <joekennedy5555@gmail.com>
2023-04-13 17:02:26 -0400

    initial test on full dataset

---
+++ PF_landmarks_real_data/run.py
@@ -0,0 +1,177 @@
+import numpy as np
+
+import yaml
+from robot_system import robot_system
+import models
+from utils import read_data, robot_sorting, calculateMeanCovFromList, getLandmarkParticles, plot
+from PF import particle_filter
+
+# settings.yaml contains many of the parameters to be tuned
+# this opens up settings.yaml and saves the data in the param object
+with open("PF_landmarks_real_data/settings.yaml", 'r') as stream:
+            param = yaml.safe_load(stream)
+
+# read_data() will read information from the .dat files and save them into numpy arrays
+data = read_data(param['max_runtime'])
+
+
+robot_list = []
+
+# create a robot system class for the number of robots you are working with
+# and add them to the robot list
+for i in range(param['num_robots']):
+    robot = robot_system()
+    robot.id = i+1
+    robot.process_model = models.process_model
+    robot.process_covariance = np.eye(3) * param['robot_process_covariance']
+    robot.process_input_size = 3
+
+    robot.measurement_model = models.measurement_model
+    robot.measurement_covariance = np.eye(2) * param['robot_measurement_covariance']
+    robot.inverse_measurement_model = models.inv_measurement_model
+
+    robot.initial_state = data.robots[i].groundtruth[0, 1:]
+    robot.initial_covariance = np.eye(3) * param['robot_initial_covariance']
+    robot.detected_landmarks_pf = {}
+
+    robot.odometry_data = data.robots[i].odometry
+    robot.measurement_data = data.robots[i].measurements
+    robot.odometry_index = 0
+    robot.check_if_reached_end_of_odometry = 0
+    robot.measurement_index = 0
+    robot.check_if_reached_end_of_measurement = 0
+
+    robot.robot_particle_color = param['robot_particle_color'][i]
+    robot.measurement_particle_color = param['measurement_particle_color'][i]
+
+    robot.pf = particle_filter(
+        process_model = robot.process_model, process_covariance = robot.process_covariance, process_input_size=robot.process_input_size,
+        measurement_model= robot.measurement_model, measurement_covariance= robot.measurement_covariance,
+        initial_state= robot.initial_state, initial_covariance= robot.initial_covariance,
+        num_particles = param['num_particles_robots'])
+
+    robot_list.append(robot)
+
+
+# main loop where we either perform a motion_step or measurement step based on 
+# the odometry or measurement data that occurs next 
+
+image_num = 0
+
+while True:
+    print(image_num)
+
+    # sort the robot list by the sorting robot function
+    # this sorts the robots in acending timestep, where the timestep is the 
+    # minimum value between the next robot odometry timestep and the 
+    # next robot measurement timestep
+    # the first robot in the sorted list will have the next move or update step
+    robot = sorted(robot_list, key=robot_sorting)[0]
+
+
+    # if the next motion or measurement step would use data from a timestep that is after max_runtime
+    # or if we have reached the end of both datasets, break from while loop
+    if (max(robot.odometry_data[robot.odometry_index, 0], robot.check_if_reached_end_of_odometry) >= param['max_runtime'] and 
+        max(robot.measurement_data[robot.measurement_index, 0], robot.check_if_reached_end_of_measurement) >= param['max_runtime']):
+        break
+
+    # check to see if the robot should take a move step or measurement step
+    if (max(robot.odometry_data[robot.odometry_index, 0], robot.check_if_reached_end_of_odometry) < 
+        max(robot.measurement_data[robot.measurement_index, 0], robot.check_if_reached_end_of_measurement)):
+        
+        # odometry timestep is sooner
+
+        # calculate input u at this timestep
+        u = np.array([robot.odometry_data[robot.odometry_index, 1], robot.odometry_data[robot.odometry_index, 2], 0.0])
+        # perform the motion step
+        robot.pf.motion_step(u)
+        
+        # if the robot did not have a forward velocity, then we will not be able to visually see the result
+        # so do not plot
+        if(u[0] != 0):
+            plot(robot_list, data, image_num, robot.odometry_data[robot.odometry_index, 0])
+            image_num += 1
+
+        robot.odometry_index += 1
+        # if index is past the end of the data, set the check_if_reached_end_of_odometry
+        if robot.odometry_index >= robot.odometry_data.shape[0]:
+            robot.check_if_reached_end_of_odometry = param['max_runtime']
+            robot.odometry_index = 0 # no longer use odometry index, ensures it is not out of scope of data
+
+
+        
+
+    else:
+          
+        # measurement timestep is sooner
+
+        # get measurement z at current timestep
+        z = np.array([robot.measurement_data[robot.measurement_index, 2], robot.measurement_data[robot.measurement_index, 3]])
+        landmark_id = int(robot.measurement_data[robot.measurement_index, 1])
+
+        # if the landmark id is less than or equal to the number of robots, you have detected a robot
+        if landmark_id <= param['num_robots']:
+            
+            # if you detect another robot
+            # 'communicate' with the robot and get the mean and covariance associated with its location
+            detected_robot = robot_list[landmark_id-1]
+            detected_robot_mean, detected_robot_covariance = calculateMeanCovFromList(detected_robot.pf.particles.state)
+            # then use this mean and variance as a measurement update
+            robot.pf.measurement_step(z, detected_robot_mean[0:2], detected_robot_covariance[0:2, 0:2])
+
+            for landmark_id, landmark_pf in robot.detected_landmarks_pf.items():
+                 if landmark_id in detected_robot.detected_landmarks_pf.keys():
+                    detected_robot_landmark_particles = detected_robot.detected_landmarks_pf[landmark_id].particles.state
+                    detected_robot_landmark_mean, detected_robot_landmark_covariance = calculateMeanCovFromList(detected_robot_landmark_particles)  
+                    robot.detected_landmarks_pf[landmark_id].measurement_step_landmarks(detected_robot_landmark_mean, detected_robot_landmark_covariance)
+
+
+        elif landmark_id in robot.detected_landmarks_pf.keys():
+
+            # if you detect a landmark you have detected before
+            # get a list of particles representing where this landmark could be based on where the robot
+            # currently thinks it is and the inverse_measurement_model
+            detected_landmark_particles = getLandmarkParticles(z, robot.inverse_measurement_model, robot.measurement_covariance, 
+                                                               robot.pf.particles.state, param['num_measurement_particles_per_robot_particle'])
+            # calculate the mean and covariance of this detected landmark, then use that as a measurement update for the landmark pf
+            current_landmark_mean, current_landmark_covariance = calculateMeanCovFromList(robot.detected_landmarks_pf[landmark_id].particles.state)
+            detected_landmark_mean, detected_landmark_covariance = calculateMeanCovFromList(detected_landmark_particles)
+            
+            #robot.detected_landmarks_pf[landmark_id].measurement_step_compare_particles(detected_landmark_particles)
+
+            # plot the detected landmark so that we can see if measurement update was reasonable
+            plot(robot_list, data, image_num, robot.measurement_data[robot.measurement_index, 0], 
+                 observed_landmark_particles = detected_landmark_particles, robot_observing = robot.id)
+            
+            #update the landmark's particle filter
+            robot.detected_landmarks_pf[landmark_id].measurement_step_landmarks(detected_landmark_mean, detected_landmark_covariance)
+            #robot.detected_landmarks_pf[landmark_id].measurement_step_compare_particles(detected_landmark_particles)
+            #robot.detected_landmarks_pf[landmark_id].measurement_step_combine_gaussians(detected_landmark_mean, detected_landmark_covariance)
+            
+            # now that the landmark has been detected at least twice, we can update our robot position based on this landmark measurement
+            landmark_mean, landmark_covariance = calculateMeanCovFromList(robot.detected_landmarks_pf[landmark_id].particles.state)
+            robot.pf.measurement_step(z, landmark_mean, landmark_covariance)
+
+        else:
+             
+            # if this is the first time you have detected the landmark
+            # get a list of particles representing where this landmark could be based on where the robot
+            # currently thinks it is and the inverse_measurement_model
+            detected_landmark_particles = getLandmarkParticles(z, robot.inverse_measurement_model, robot.measurement_covariance, 
+                                                               robot.pf.particles.state, param['num_measurement_particles_per_robot_particle'])
+            # create a particle filter associated with this landmark and add it to the robot.detected_landmarks_pf list
+            robot.detected_landmarks_pf[landmark_id] = particle_filter( 
+                given_starting_particles = detected_landmark_particles)
+
+        
+        # plot the measurement step
+        plot(robot_list, data, image_num, robot.measurement_data[robot.measurement_index, 0])
+        image_num += 1
+
+        robot.measurement_index += 1
+        # if index is past the end of the data, set the check_if_reached_end_of_measurement
+        if robot.measurement_index >= robot.measurement_data.shape[0]:
+            robot.check_if_reached_end_of_measurement = param['max_runtime']
+            robot.measurement_index = 0 # no longer use measurement index, ensures it is not out of scope of data
+
+        
---
+++ PF_landmarks_real_data/utils.py
@@ -0,0 +1,283 @@
+import numpy as np
+import os
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
+
+class data_structure:
+    def __init__(self):
+        self.landmark_groundtruth = None
+        self.robots = []
+
+class robot_data_structure:
+    def __init__(self):
+        self.groundtruth = None
+        self.measurements = None
+        self.odometry = None
+
+def read_data(max_runtime):
+    data = data_structure()
+
+    landmark_groundtruth_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Landmark_Groundtruth.dat'))
+    robot1_groundtruth_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Robot1_Groundtruth.dat'))
+    robot1_measurement_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Robot1_Measurement.dat'))
+    robot1_odometry_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Robot1_Odometry.dat'))
+
+    robot2_groundtruth_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Robot2_Groundtruth.dat'))
+    robot2_measurement_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Robot2_Measurement.dat'))
+    robot2_odometry_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'UTIAS-dataset/MRCLAM_Dataset1', 'Robot2_Odometry.dat'))
+
+    data.landmark_groundtruth = np.loadtxt(landmark_groundtruth_path)
+
+
+    robot1 = robot_data_structure()
+    robot1.groundtruth = np.loadtxt(robot1_groundtruth_path)
+    robot1.measurements = np.loadtxt(robot1_measurement_path)
+    robot1.odometry = np.loadtxt(robot1_odometry_path)
+
+    for i in range(robot1.groundtruth.shape[0]):
+        if robot1.groundtruth[i,0] > max_runtime:
+            robot1.groundtruth = robot1.groundtruth[0:i, :]
+            break
+    for i in range(robot1.measurements.shape[0]):
+        if robot1.measurements[i,0] > max_runtime:
+            robot1.measurements = robot1.measurements[0:i, :]
+            break
+    for i in range(robot1.odometry.shape[0]):
+        if robot1.odometry[i,0] > max_runtime:
+            robot1.odometry = robot1.odometry[0:i, :]
+            break
+
+    robot2 = robot_data_structure()
+    robot2.groundtruth = np.loadtxt(robot2_groundtruth_path)
+    robot2.measurements = np.loadtxt(robot2_measurement_path)
+    robot2.odometry = np.loadtxt(robot2_odometry_path)
+
+    for i in range(robot2.groundtruth.shape[0]):
+        if robot2.groundtruth[i,0] > max_runtime:
+            robot2.groundtruth = robot2.groundtruth[0:i, :]
+            break
+    for i in range(robot2.measurements.shape[0]):
+        if robot2.measurements[i,0] > max_runtime:
+            robot2.measurements = robot2.measurements[0:i, :]
+            break
+    for i in range(robot2.odometry.shape[0]):
+        if robot2.odometry[i,0] > max_runtime:
+            robot2.odometry = robot2.odometry[0:i, :]
+            break
+
+    data.robots = [robot1, robot2]
+
+
+    return(data)
+
+def calculateMeanCovFromList(particle_list):
+    """
+    particle_array = np.array(particle_list)
+    particle_mean = np.sum(particle_array, axis=0) / particle_array.shape[0]
+
+    zero_mean = particle_array - np.tile(particle_mean, (particle_array.shape[0], 1))
+    
+
+    particle_covariance = zero_mean.T @ zero_mean / particle_array.shape[0]
+
+    # check to raise error if not positive definite
+    np.linalg.cholesky(particle_covariance)
+    print("ran calculateMeanCovFromList correctly")
+
+
+    return particle_mean, particle_covariance
+    """
+    """
+    particle_array = np.array(particle_list)
+
+    #particle_mean = np.sum(particle_array, axis=0) / particle_array.shape[0]
+    particle_mean = np.mean(particle_array, axis = 0)
+    print(particle_array)
+    print(particle_mean)
+    print(particle_mean.shape[0])
+
+    if particle_mean.shape[0] == 3:
+        sinSum = 0
+        cosSum = 0
+        for s in range(particle_array.shape[0]):
+            cosSum += np.cos(particle_array[s,2])
+            sinSum += np.sin(particle_array[s,2])
+        particle_mean[2] = np.arctan2(sinSum, cosSum)
+
+    zero_mean = np.zeros_like(particle_array) 
+    for s in range(particle_array.shape[0]):
+        zero_mean[s,:] = particle_array[s,:] - particle_mean
+        if particle_mean.shape[0] == 3:
+            zero_mean[s,2] = wrap2Pi(zero_mean[s,2]) 
+
+    particle_covariance = zero_mean.T @ zero_mean / particle_array.shape[0]
+    np.linalg.cholesky(particle_covariance)
+    print("ran calculateMeanCovFromList correctly")
+    print(particle_covariance)
+
+    return particle_mean, particle_covariance
+    """
+    # to ensure mean covariance are correct, transpose array and then use code from HW5
+    particle_array = np.array(particle_list)
+    particle_array = particle_array.T
+    particle_mean = np.mean(particle_array, axis=1)
+    if particle_mean.shape[0] == 3: # only mess with heading if this is a robot pose
+        sinSum = 0
+        cosSum = 0
+        for s in range(particle_array.shape[1]):
+            cosSum += np.cos(particle_array[2,s])
+            sinSum += np.sin(particle_array[2,s])
+        particle_mean[2] = np.arctan2(sinSum, cosSum)
+    zero_mean = np.zeros_like(particle_array)
+    for s in range(particle_array.shape[1]):
+        zero_mean[:,s] = particle_array[:,s] - particle_mean
+        if particle_mean.shape[0] == 3: # only mess with heading if this is a robot pose
+            zero_mean[2,s] = wrap2Pi(zero_mean[2,s])
+    particle_covariance = zero_mean @ zero_mean.T / particle_array.shape[1]
+    
+    return particle_mean, particle_covariance
+
+
+def wrap2Pi(input):
+    phases =  (( -input + np.pi) % (2.0 * np.pi ) - np.pi) * -1.0
+
+    return phases
+
+
+def robot_sorting(robot):
+
+    return min(max(robot.odometry_data[robot.odometry_index, 0], robot.check_if_reached_end_of_odometry), 
+               max(robot.measurement_data[robot.measurement_index, 0], robot.check_if_reached_end_of_measurement))
+
+def getLandmarkParticles(z, inv_measurement_model, measurement_covariance, robot_particles, num_particles_per_robot_particle = 10):
+    
+    list_landmark_particles = []
+    measurement_covariance_L = np.linalg.cholesky(measurement_covariance)
+
+    for particle in robot_particles:
+            detected_landmark_location = inv_measurement_model(particle.squeeze(), z)
+
+            for i in range (num_particles_per_robot_particle):
+                sample_measurement_noise = measurement_covariance_L @ np.random.randn(z.shape[0],1)
+                list_landmark_particles.append(detected_landmark_location + sample_measurement_noise.squeeze())
+    
+    return list_landmark_particles
+
+"""    
+def plot_covariance(ax, mean, cov):
+    xy = (mean[0], mean[1])
+    a = cov[0, 0]
+    b = cov[0, 1]
+    c = cov[1, 1]
+
+    term_1 = (a + c) / 2
+    term_2 = np.sqrt((((a - c) / 2) ** 2) + b ** 2)
+
+    lambda_1 = term_1 + term_2
+    lambda_2 = term_1 - term_2
+
+    if b == 0:
+        if a >= c:
+            theta = 0
+        else:
+            theta = 45
+    else:
+        theta = np.arctan2(lambda_1 - a, b)
+
+    ellipse = Ellipse(xy, width=2 * lambda_1, height=2 * lambda_2, angle=theta, fill=False, color='red')
+    ax.add_patch(ellipse)
+"""
+def plot_covariance(ax, mean, covariance):
+    covariance = covariance[0:2, 0:2]
+    eigenvalues, eigenvectors = np.linalg.eig(covariance)
+    if eigenvalues[0] > eigenvalues[1]:
+        largest_eigenvalue = eigenvalues[0]
+        smallest_eigenvalue= eigenvalues[1]
+        largest_eigenvector = eigenvectors[:,0]
+        smallest_eigenvector = eigenvectors[:,1]
+    else:
+        largest_eigenvalue = eigenvalues[1]
+        smallest_eigenvalue = eigenvalues[0]
+        largest_eigenvector = eigenvectors[:,1].reshape(-1)
+        smallest_eigenvector = eigenvectors[:,0].reshape(-1)
+    # calulate angle between largest eigenvector and x_axis
+    angle = np.arctan2(largest_eigenvector[1], largest_eigenvector[0])
+    if angle < 0:
+        angle = angle + 2*np.pi
+
+    # get the 95% confidence interval error ellipse
+    chisquare_val = 2.4477
+    theta_grid = np.linspace(0,2*np.pi, 100)
+    phi = angle
+    a = chisquare_val * np.sqrt(largest_eigenvalue)
+    b = chisquare_val * np.sqrt(smallest_eigenvalue)
+    # elipse in x and y coordinates
+    ellipse_x_r  = a*np.cos( theta_grid )
+    ellipse_y_r  = b*np.sin( theta_grid )
+
+    #Define a rotation matrix
+    R = np.array([[np.cos(phi), np.sin(phi)],[ -np.sin(phi), np.cos(phi)]])
+
+    #let's rotate the ellipse to some angle phi
+    r_ellipse = np.vstack((ellipse_x_r,ellipse_y_r)).T @ R
+    ax.plot(r_ellipse[:,0] + mean[0], r_ellipse[:,1] + mean[1], c='k')
+
+
+def plot(robots, data, image_num, current_time, observed_landmark_particles=None, robot_observing = None):
+
+    path_to_images = os.path.realpath(os.path.join(os.path.dirname(__file__), 'saved_images'))
+    if os.path.exists(path_to_images) == False:
+       os.mkdir(path_to_images) 
+
+    fig, axs = plt.subplots(len(robots))
+    fig.tight_layout(h_pad=2)
+
+    robot_groundtruth_colors = ['r', 'y', 'g', 'b', 'm']
+
+    for i, robot in enumerate(robots):
+
+        axs[i].set_title("R" + str(i+1) + ": dark_color = robot, light_color= landmark, orange = observed")
+        
+        landmarks_x = data.landmark_groundtruth[:, 1]
+        landmarks_y = data.landmark_groundtruth[:,2]
+        axs[i].scatter(landmarks_x, landmarks_y, c='k', marker = "*")
+
+        for j, robot_groundtruth_data in enumerate(data.robots):
+            robot_groundtruth_x = robot_groundtruth_data.groundtruth[:,1]
+            robot_groundtruth_y = robot_groundtruth_data.groundtruth[:,2]
+            axs[i].plot(robot_groundtruth_x, robot_groundtruth_y, robot_groundtruth_colors[j])
+
+            robot_particles_x = (np.array(robot.pf.particles.state))[:,0]
+            robot_particles_y = (np.array(robot.pf.particles.state))[:,1]
+            axs[i].scatter(robot_particles_x, robot_particles_y, s=1, c= robot.robot_particle_color)
+            robot_mean, robot_cov = calculateMeanCovFromList(robot.pf.particles.state)
+            plot_covariance(axs[i], robot_mean, robot_cov)
+
+        for landmark_id, landmark_pf in robot.detected_landmarks_pf.items():
+            landmark_particles_x = (np.array(landmark_pf.particles.state))[:,0]
+            landmark_particles_y = (np.array(landmark_pf.particles.state))[:,1]
+            axs[i].scatter(landmark_particles_x, landmark_particles_y, s=1, c=robot.measurement_particle_color)
+            landmark_mean, landmark_cov = calculateMeanCovFromList(landmark_pf.particles.state)
+            plot_covariance(axs[i], landmark_mean, landmark_cov)
+
+
+
+    if observed_landmark_particles is None:
+        plt.savefig(path_to_images + "/image_" + str(image_num) +"_time_" + str(current_time) + "_result.png")
+        plt.close()
+
+    else:
+
+        observed_landmark_particles_x = (np.array(observed_landmark_particles))[:,0]
+        observed_landmark_particles_y = (np.array(observed_landmark_particles))[:,1]
+
+        axs[robot_observing - 1].scatter(observed_landmark_particles_x, observed_landmark_particles_y, s=1, c='#ed8026', marker = "x")
+        observed_landmark_mean, observed_landmark_cov = calculateMeanCovFromList(observed_landmark_particles)
+        plot_covariance(axs[robot_observing - 1], observed_landmark_mean, observed_landmark_cov)
+
+        plt.savefig(path_to_images + "/image_" + str(image_num) +"_time_" + str(current_time) + "_landmark_update.png")
+        plt.close()
+        
